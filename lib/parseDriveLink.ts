d09c3cf1711d2a7079a6da95dbe51770a4922748
EnochMaharishi <40563178+EnochMaharishi@users.noreply.github.com>
2025-12-14 23:57:36 +0000

    Update parseDriveLink.ts

--- lib/parseDriveLink.ts
+++ lib/parseDriveLink.ts
@@ -1,4 +1,5 @@
 export function parseDriveLink(url: string): string | null {
+  https://drive.google.com/drive/u/0/folders/1ZLcyNHGRmGsuFJpRjLsmsKfohlxP7-Pv
   const match =
     url.match(/folders\/([a-zA-Z0-9_-]+)/) ||
     url.match(/[?&]id=([a-zA-Z0-9_-]+)/);
