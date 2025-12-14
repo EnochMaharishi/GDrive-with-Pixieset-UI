def4c01c766ea5397174d24c0bc8ef43cfb8fd0a
EnochMaharishi <40563178+EnochMaharishi@users.noreply.github.com>
2025-12-14 23:35:12 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import DriveLinkForm from "@/components/DriveLinkForm";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <DriveLinkForm />
+    </main>
+  );
+}
